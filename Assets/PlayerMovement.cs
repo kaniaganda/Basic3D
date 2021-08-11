a5cc39d29cb4bc08ec1c05925cb41e43a35642eb
Kania Ganda <kaniagandacs@gmail.com>
2021-08-10 23:26:15 -0700

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // A reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+    
+    public float forwardForce = 2000f; // Unity sometimes wants "f" added for floats
+    public float sidewaysForce = 500f;
+
+    // FixUpdate used when adjusting physics in Unity
+    void FixedUpdate()
+    {
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); 
+
+        if (Input.GetKey("d")) 
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a")) 
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
